0b9c7c17a17663e0536069e2215d64a6624eb75c
shailesh2210 <shaileshgaddam10@gmail.com>
2026-03-10 21:50:48 +0530

    Data Ingestion First step completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,27 @@
 from src.logger.logger import logging
+from pathlib import Path
+import pandas as pd
+from dataclasses import dataclass
+import os
 
 print("Hello World!")
 logging.info("Logging has Started")
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join("Artifacts", "train_data.csv")
+    test_data_path = os.path.join("Artifacts", "test_data.csv")
+    raw_data_path = os.path.join("Artifacts", "train_data.csv")
+
+class DataIngestion:
+
+    def __init__(self):
+        self.data_ingestion = DataIngestionConfig
+
+    def initiate_data_ingestion(self):
+        
+        try:
+            data = pd.read_csv(os.path.join("data/", "raw_data.csv"))
+            
+        except:
+            pass
